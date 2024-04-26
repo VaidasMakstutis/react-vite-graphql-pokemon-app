912f5d3f0033751d73e1eeb06e3fbe6008354f3b
Vaidas <vaidas.makstutis@gmail.com>
2024-04-26 13:07:19 +0300

    Container component

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useQuery } from "@apollo/client";
+import Pokemon from "./Pokemon";
+import { GET_POKEMONS } from "../graphql/get-pokemons";
 
 const Container = () => {
-  return <h2>Container Component</h2>;
+  const { loading, error, data } = useQuery(GET_POKEMONS, {
+    variables: { first: 5 }
+  });
+
+  if (loading) return <p>Loading data...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const pokemons = data?.pokemons || [];
+
+  return <div className="container">{pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}</div>;
 };
 
 export default Container;

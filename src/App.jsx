52e74a6dc6e4f8fa1d7f5b42d0f9e1ea453edde0
Vaidas <vaidas.makstutis@gmail.com>
2024-04-26 12:15:00 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./components/Container";
+import "./App.css";
+
+const App = () => {
+  return (
+    <main>
+      <Container />
+    </main>
+  );
+};
+
+export default App;

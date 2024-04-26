f89194767acdf8bd7b7187c29c1fa14600115fb2
Vaidas <vaidas.makstutis@gmail.com>
2024-04-26 13:29:19 +0300

    Added tailwind css to project

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -7,7 +7,7 @@ const Container = () => {
     variables: { first: 5 }
   });
 
-  if (loading) return <p>Loading data...</p>;
+  if (loading) return <p className="text-3xl font-bold">Loading data...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
   const pokemons = data?.pokemons || [];

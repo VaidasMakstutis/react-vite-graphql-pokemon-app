d5423dc51c7794340afcbc215bd37d73b55e614e
Vaidas <vaidas.makstutis@gmail.com>
2024-04-26 22:31:19 +0300

    Responsive design

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,16 +4,18 @@ import { GET_POKEMONS } from "../graphql/get-pokemons";
 
 const Container = () => {
   const { loading, error, data } = useQuery(GET_POKEMONS, {
-    variables: { first: 5 }
+    variables: { first: 7 }
   });
 
-  if (loading) return <p className="text-2xl font-bold text-blue-700 text-center m-5">Loading data...</p>;
+  if (loading) return <p className="text-2xl font-semibold text-blue-600 text-center m-5">Loading data...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
   const pokemons = data?.pokemons || [];
 
   return (
-    <div className="container gap-10 m-5 grid grid-cols-3">{pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}</div>
+    <div className="container grid gap-2 sm:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4 m-3">
+      {pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+    </div>
   );
 };
 
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -2,20 +2,20 @@ import PropTypes from "prop-types";
 
 const Pokemon = ({ pokemon }) => {
   return (
-    <div className="pokemon border-solid rounded-md border-2 border-gray-300">
-      <div className="pokemon__name bg-yellow-400 rounded-md font-bold text-center text-xl text-white uppercase py-2">
+    <div className="border-solid border-2 border-gray-300 rounded-md">
+      <div className="bg-yellow-500 text-xl font-bold text-white text-center uppercase rounded-md py-2">
         <p>{pokemon.name}</p>
       </div>
-      <div className="pokemon__meta grid grid-cols-2 gap-10 m-3">
-        <span className="text-white text-center font-bold bg-green-400 rounded-md py-1">maxHP: {pokemon.maxHP}</span>
-        <span className="text-white text-center font-bold bg-green-400 rounded-md py-1">maxCP: {pokemon.maxCP}</span>
+      <div className="grid grid-cols-2 gap-10 m-3">
+        <span className="bg-green-500 text-md font-normal text-white text-center rounded-md py-1">maxHP: {pokemon.maxHP}</span>
+        <span className="bg-green-500 text-md font-normal text-white text-center rounded-md py-1">maxCP: {pokemon.maxCP}</span>
       </div>
-      <div className="pokemon__image flex justify-center items-center p-5">
+      <div className="flex justify-center items-center p-5">
         <img className="size-48" src={pokemon.image} alt={pokemon.name} title={pokemon.name} />
       </div>
-      <div className="pokemon__attacks grid grid-cols-3 py-2">
+      <div className="grid gap-1 grid-cols-3 p-1">
         {pokemon.attacks.special.slice(0, 3).map(attack => (
-          <span className="bg-red-500 font-bold text-center text-white m-2 p-1 rounded-md" key={`${attack.name}-${attack.damage}`}>
+          <span className="bg-red-500 text-sm font-normal text-white text-center rounded-md m-1 p-1" key={`${attack.name}-${attack.damage}`}>
             {attack.name}
           </span>
         ))}
